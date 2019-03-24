410477d69301927d6bcc3e20ed35498b5884bb46
Marat Nepomnyashy <maratbn@gmail.com>
2019-03-24 12:13:29 -0700

    Added initial build.

---
+++ precache-manifest.fced5d2e835ec583820c55191808ac2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d56b1f1530896afa12bd",
+    "url": "/static/js/main.c2a91cd7.chunk.js"
+  },
+  {
+    "revision": "08044e226e8b59b41bce",
+    "url": "/static/js/2.78793119.chunk.js"
+  },
+  {
+    "revision": "d56b1f1530896afa12bd",
+    "url": "/static/css/main.b3fc79a4.chunk.css"
+  },
+  {
+    "revision": "a0c133b0221c0580347421225975764d",
+    "url": "/index.html"
+  }
+];
